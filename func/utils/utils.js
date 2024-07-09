8822d58e4780edf6ec461150064774faad4f0e59
rontea <dupelawebsite@gmail.com>
2024-07-09 10:13:15 -0400

    util update

--- func/utils/utils.js
+++ func/utils/utils.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const readline = require('readline');
 const path = require('path');
 
 function twhUrlPathMaker(url = {}){
@@ -21,5 +22,51 @@ function test(){
     console.log("Test");
 }
 
+function writeLine(message) {
+    console.log(message);
+}
 
-module.exports = {twhUrlPathMaker , test};
+/**
+ * This will confirm user input
+ * @returns Promise
+ */
+
+async function readLineYesNoSync(){
+
+    try {
+        /** return promise for the await to function */
+        return new Promise(resolve => {
+          const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+          });
+    
+          rl.question("Are you use you want to continue y/n : " , 
+            (answer) => {
+              rl.close();
+    
+              if(answer.toLowerCase() === 'y') {
+                
+                console.log("User execute operation.");
+                resolve(true);
+  
+              }else if(answer.toLowerCase() === 'n'){
+                
+                console.log("User cancelled operation.");
+                resolve(false);
+    
+              }else {
+                console.log("User input invalid.")
+                resolve(false);
+              }
+    
+          });
+        });
+  
+      }catch(err) {
+        console.lof("Confirmation error :" , err);
+      }
+
+}
+
+module.exports = {twhUrlPathMaker , test , writeLine, readLineYesNoSync};
