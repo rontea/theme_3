ff601dd2258081f8ca176626df4899df5665d9f7
DupelaWebsite <dupelawebsite@gmail.com>
2025-09-09 14:53:12 +0000

    ci(chore(ci): update github action and releaserc): github action update and releaserc.js update

--- .releaserc.js
+++ .releaserc.js
@@ -1,14 +1,20 @@
 module.exports = {
-    branches: [
-      'main',
-      { name: 'next', prerelease: false }
+  branches: [
+    'main',
+    { name: 'next', prerelease: 'next' }, // prerelease channel (e.g., 1.0.1-next.1)
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    ...(process.env.NPM_PUBLISH === 'true' ? ['@semantic-release/npm'] : []),
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
     ],
-    plugins: [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator',
-      '@semantic-release/changelog',
-      ...(process.env.NPM_PUBLISH === 'true' ? ['@semantic-release/npm'] : []),
-      '@semantic-release/github',
-      '@semantic-release/git'
-    ]
+  ],
 };
